ad5508996287eebdde795d662132055703a7b7f8
Edgar <ed-hovsepyan@mail.ru>
2017-09-03 15:01:02 +0400

    some changes

--- New_class.js
+++ New_class.js
@@ -1,6 +1,7 @@
 class MyCar{
-  constructor(brand, engine, power, color){
+  constructor(brand, model, engine, power, color){
     this.brand = brand;
+    this.model = model;
     this.engine = engine;
     this.power = power;
     this.color = color;
@@ -19,17 +20,18 @@ class MyCar{
   }
 }
 
-let my_Car = new MyCar("Opel", [1.8], [115, "Hp"],"grey"); 
+let my_Car = new MyCar("Opel","Vectra", [1.8], [115, "Hp"],"grey"); 
 my_Car.setCost(3250);
 console.log(my_Car);
-let friendCar = new MyCar("Nissan", [2.3], [173, "Hp"],"white");
+
+let friendCar = new MyCar("Nissan","Teana", [2.3], [173, "Hp"],"white");
 friendCar.setCost(4400)
 friendCar.opinion();
 console.log(friendCar);
 
 class dreamingCar extends MyCar{
-  constructor(brand, engine, power, color, maxSpeed, tireSize){
-    super(brand, engine, power, color);
+  constructor(brand, model, engine, power, color, maxSpeed, tireSize){
+    super(brand, model, engine, power, color);
     this.maxSpeed = maxSpeed;
     this.tireSize = tireSize;
 
@@ -38,10 +40,24 @@ class dreamingCar extends MyCar{
 work() {
     console.log('Powerfull car');
 
-}
+  }
 }
 
-let dreamCar = new dreamingCar("BMW", [4.4], [575, "Hp"], "white", [250,"km/h"], "R-19");
+let dreamCar = new dreamingCar("BMW", "M5",[4.4], [575, "Hp"], "white", [250,"km/h"], "R-19");
 dreamCar.setCost(8000);
 dreamCar.work();
 console.log(dreamCar);
+
+class economyCar extends dreamingCar{
+  constructor(brand, model, engine, power, color, maxSpeed, tireSize, spareParts){
+    super(brand,model, engine, power, color, maxSpeed, tireSize);
+    this.spareParts = spareParts;
+  }
+  opinion(){
+    console.log("This car Fuel Economy is amazing")
+  }
+}
+
+let economy_Car = new economyCar ("Toyota", "Corolla", [1.6], [132, "Hp"], "dark grey", [290,"km/h"], "R16", "high quality");
+economy_Car.opinion();
+console.log(economy_Car)
